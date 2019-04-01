dba72c4ecfd90616959ebe02b6f170b63c52d688
shubhamj25 <49161473+shubhamj25@users.noreply.github.com>
2019-04-01 16:18:53 +0530

    My First Website

---
+++ index.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="custom.css">
+
+    <title>Quick Pick</title>
+  </head>
+  <body><div id="body">
+    <nav class="navbar navbar-expand navbar-transparent fixed-top" style="width: 100%;" id="navbar">
+  <a class="navbar-brand" href="#" style="font-size: 1.5vw;"><b>Quick Pick Services</b></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#" style="font-size: 1.5vw;">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="font-size: 1.5vw;">Sign In</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" style="font-size: 1.5vw;">Worship Technology & re-define your own prayers !</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style="font-size: 1.5vw;width: 15vw;height: 3.1vw">
+      <button class="btn btn-success my-2 my-sm-0" type="submit" style="font-size: 1.5vw;width: 10vw;height: 3.1vw;padding-bottom: 2.7vw;"><center>Search</center></button>
+    </form>
+  </div>
+</nav>
+ <div id="carouselExample" class="carousel slide" data-ride="carousel" style="width: 100%;">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExample" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExample" data-slide-to="1"></li>
+    <li data-target="#carouselExample" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner" onmouseover="document.getElementById('demo').style.height='44.4vw';document.getElementById('demo').style.transition='1s ease';document.getElementById('demo1').style.height='44.4vw';document.getElementById('demo1').style.transition='1s ease';document.getElementById('demo2').style.height='44.4vw';document.getElementById('demo2').style.transition='1s ease';">
+    <div class="carousel-item active" data-interval="10000" id="demo" style="height:44.4vw;">
+      <img src="image1.jpg" class="d-block w-100" alt="image1"><div class="carousel-caption" style="font-size: 1.5vw;text-align: left;padding-right: 27vw;padding-top: 20px;"><h4>First button</h4><p>
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid.<br><br><button class="btn btn-success" style="font-size: 1.5vw;">School Services</button>
+      </p></div>
+    </div>
+    <div class="carousel-item" data-interval="10000" id="demo1" style="height:44.4vw;">
+      <img src="image2.jpg" class="d-block w-100" alt="image2"><div class="carousel-caption" style="font-size: 1.5vw;text-align: left;padding-right: 27vw;color: black;padding-top: 20px;"><h4>Second Button</h4><p>
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid.<br><br><button class="btn btn-success" style="font-size: 1.5vw;">Daily Services</button>
+      </p></div>
+    </div>
+    <div class="carousel-item" data-interval="10000" id="demo2" style="height:44.4vw;">
+      <img src="image3.jpg" class="d-block w-100" alt="image3"><div class="carousel-caption" style="font-size: 1.5vw;text-align: left;padding-right: 27vw;color: black;padding-top: 20px;"><h4>Third Slide</h4><p>
+      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid.<br><br><button class="btn btn-success" style="font-size: 1.5vw;">Shopping Services</button>
+    </p></div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div class="accordion" id="accordionExample" >
+  <div class="card">
+    <div class="card-header" id="headingOne" style="padding:0px;border-radius: 0px;">
+      <h2 class="mb-0"><center>
+        <button class="btn btn-lg btn-block" style="border-radius:0px;font-size:1.5vw;color:white;background-color: black;" type="button" data-toggle="collapse" onclick="document.getElementById('demo').style.height='28vw';document.getElementById('demo').style.transition='1s ease';document.getElementById('demo1').style.height='28vw';document.getElementById('demo1').style.transition='1s ease';document.getElementById('demo2').style.height='28vw';document.getElementById('demo2').style.transition='1s ease';" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Services Available
+        </button></center>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse hide" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body" style="padding: 40px 100px 20px 100px;height: auto;">
+        <center>
+      	<button class="btn btn-info" style="border-radius:3vw;padding-right: 1.3vw;padding-left: 1.3vw;font-size: 1.5vw;">School Services</button>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<button class="btn btn-warning" style="border-radius:3vw;padding-right: 1.3vw;padding-left: 1.3vw;font-size: 1.5vw;">Daily Services</button>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<button class="btn btn-danger" style="border-radius:3vw;padding-right: 1.3vw;padding-left: 1.3vw;font-size: 1.5vw;">Shopping Services</button>
+      <br><p style="padding:1vw 14vw 1vw 14vw;font-size: 1.5vw;">
+      For Membership related queries pleases visit the service page of the services you want membership and there you will find the relative details of the pricing and payment. <br><hr style="width: 70%;">
+      </p></center>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="media" style="padding:1vw 3vw 3vw 3vw;margin-left: -1vw;">
+  <img src="icon1.jpg" height="7%" width="14%">
+  <div class="media-body" style="font-size: 1.2vw;" >
+    <p class="mt-0" style="padding-top: 2vw;text-align: justify;font-size: 1.5vw;">Why should you join us?</p>
+    This is a not a society unlike others it forms teams to work upon a project after they have completed training from our team members and provides each of the teams an opportunity to exhibit their ideas to companies and get paid in return if they get approval from any of them.Only fools will turn their back to such a great offer :) 
+  </div>
+  <img src="icon2.jpg" height="7%" width="7%">
+  <div class="media-body" style="font-size: 1.2vw;">
+    <p class="mt-0" style="padding-top: 2vw;text-align: justify;font-size: 1.5vw;">Benifits</p>
+    Perks and Certificate would be issued to each individual and the group might recive a payment from the investors if their project is selected.The basic idea is to brush up each and everyone's mind capable enough to identifiy their own intrests.
+</div>
+</div>
+
+
+
+
+ <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </div></body>
+</html>
